f091e66cc5c8526c74e5f5a80454e32d7a0e6014
Andreas Bilke <andreas@bilke.org>
2025-12-01 08:45:25 +0100

    Let the game begins

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code 2025
+
+For the past three years, I learned [Rust](https://www.rust-lang.org/) by solving [AoC puzzles](https://adventofcode.com/).
+
+Now I forgot much about Rust and I will start over again.
+
---
+++ new_day.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <day>"
+    exit 1
+fi
+
+DAY=$1
+
+mkdir "day-$DAY"
+cd "day-$DAY"
+cargo new "puzzle-1"
+cd puzzle-1
+
+cat <<EOF > src/main.rs
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        panic!("Not enough command line arguments");
+    }
+    
+    let input = &args[1];
+    let lines = read_file(input);
+    let result = process(&lines);
+    
+    println!("Result is {}", result);
+}
+
+pub fn read_file(file_name: &String) -> Vec<String> {
+    let lines = fs::read_to_string(file_name)
+        .expect("Could not read file");
+
+    let lines: Vec<String> = lines
+        .trim()
+        .split('\n')
+        .map(String::from)
+        .collect();
+    
+    lines
+}
+
+pub fn process(lines: &Vec<String>) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part() {
+        let result = process(&read_file(&String::from("../test-input")));
+
+        assert_eq!(result, 1);
+    }
+}
+EOF
+
